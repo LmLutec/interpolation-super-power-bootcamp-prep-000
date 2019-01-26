a2800edc9ac684d6ca78bc21af5425a205ce029b
jolly-hardware-0578 <lmccoy8911@gmail.com>
2019-01-26 23:04:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(col)
+  puts "R: red"
+  puts "O: orange"
+  puts "Y: yellow"
+  puts "G: green"
+  puts "B: blue"
+  puts "I: indigo"
+  puts "V: violet"
+end
+
+
+  
